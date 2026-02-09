3242f5698bc011cf4ceb690bdfbdc57e1a57c7ef
sunghun05 <sunghun051107@gmail.com>
2026-02-09 12:43:37 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.md'],
-  // base: '/repo-name/', // TODO: Update this to match your GitHub repository name if deploying to a project page
+  base: '/',
 })
